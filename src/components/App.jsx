4586c6069fc25effa1a45acf5b0a8da93bebfd45
alexandrakst <alexandra.kostenko@hotmail.com>
2023-03-29 00:24:47 -0400

    final update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+
+export default class App extends Component {
+  state = {
+    searchInput: '',
+  };
+
+  handleFormSubmit = searchInput => {
+    this.setState({ searchInput });
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery searchInput={this.state.searchInput} />
+      </div>
+    );
+  }
+}
